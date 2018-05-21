8da76e0bf4794e076f231b4c6faef899f5099b78
Jarrad Middleton <jarrad@vi.com.au>
2018-05-21 16:22:03 +1000

    added sample global config file, modified setup.js to work with new file

--- setup.js
+++ setup.js
@@ -1,6 +1,13 @@
 'use strict';
+
 var fs = require('fs');
 fs.createReadStream('.sample-env')
   .pipe(fs.createWriteStream('.env'));
-fs.createReadStream('.sample-email-sender-cli-user-config')
-  .pipe(fs.createWriteStream(process.env.HOME + "/.email-sender-cli-user-config"));
+
+if (!fs.existsSync(process.env.HOME + "/.email-sender-cli-user-config")) {
+  fs.createReadStream('.sample-email-sender-cli-user-config')
+    .pipe(fs.createWriteStream(process.env.HOME + "/.email-sender-cli-user-config"));
+  console.log("*** New email sender cli user config file written to", process.env.HOME);  
+} else {
+  console.log("*** Global config file already exists in home directory");
+}
